44a9570b9e926f74356e4361c38093aa55fdea08
Ujjwal Suman <ujjwalSuman007@gmail.com>
2021-06-14 00:04:25 +0530

    1st

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { frangment } from "react";
+import { useSelector } from "react-redux";
+
+import Counter from "./components/Counter";
+import Header from "./components/Header";
+import Auth from "./components/Auth";
+import UserProfile from "./components/UserProfile";
+
+function App() {
+  const isAuth = useSelector((state) => state.auth.isAuthenticated);
+  return (
+    <frangment>
+      <Header />
+      {!isAuth && <Auth />}
+      {isAuth && <UserProfile />}
+      <Counter />
+    </frangment>
+  );
+}
+
+export default App;
